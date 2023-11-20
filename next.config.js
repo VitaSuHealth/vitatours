bbc199d17ba2a713b7f31d8195443d39ba4b72a9
Benjamin Egers <b.egers@unasat.sr>
2023-11-19 23:42:35 -0300

    test

--- next.config.js
+++ next.config.js
@@ -4,9 +4,7 @@ const nextConfig = {
         domains: ["images.ctfassets.net"]
     },
     env:{
-        CONTENTFUL_SPACE_ID: "l5ga6ot6r2vv",
-        CONTENTFUL_ACCES_KEY: "HokQja3YmE2SrqcieX-VKM7TDD0ibWleJvtt9Vb80wg",
-        CONTENTFUL_ENVIRONMENT: "master",
+
     }
 }
 
